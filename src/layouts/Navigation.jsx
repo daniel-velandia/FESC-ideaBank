b3023c51cb1e70268b35a21850a8157e4f8d42fe
Jesus Arturo Yañez Patiño <jesusmitesoro2004@gmail.com>
2024-01-19 16:55:45 -0500

    Instalacion de bootstrap y implementacion de un navegador por añadir estilos

--- src/layouts/Navigation.jsx
+++ src/layouts/Navigation.jsx
@@ -13,6 +13,42 @@ const Navigation = () => {
   const dispatch = useDispatch();
 
   return (
+    <>
+    <div classname="container">
+  <header classname="border-bottom lh-1 py-3">
+    <div classname="row flex-nowrap justify-content-between align-items-center">
+      <div classname="col-4 pt-1">
+        <a classname="link-secondary" href="#">Subscribe</a>
+      </div>
+      <div classname="col-4 text-center">
+        <a classname="blog-header-logo text-body-emphasis text-decoration-none" href="#">Large</a>
+      </div>
+      <div classname="col-4 d-flex justify-content-end align-items-center">
+        <a classname="link-secondary" href="#" aria-label="Search">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" classname="mx-3" role="img" viewBox="0 0 24 24"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"/><path d="M21 21l-5.2-5.2"/></svg>
+        </a>
+        <a classname="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
+      </div>
+    </div>
+  </header>
+
+  <div classname="nav-scroller py-1 mb-3 border-bottom">
+    <nav classname="nav nav-underline justify-content-between">
+      <a classname="nav-item nav-link link-body-emphasis active" href="#">World</a>
+      <a classname="nav-item nav-link link-body-emphasis" href="#">U.S.</a>
+      <a classname="nav-item nav-link link-body-emphasis" href="#">Technology</a>
+      <a classname="nav-item nav-link link-body-emphasis" href="#">Design</a>
+      <a classname="nav-item nav-link link-body-emphasis" href="#">Culture</a>
+      <a classname="nav-item nav-link link-body-emphasis" href="#">Business</a>
+      <a classname="nav-item nav-link link-body-emphasis" href="#">Politics</a>
+      <a classname="nav-item nav-link link-body-emphasis" href="#">Opinion</a>
+      <a classname="nav-item nav-link link-body-emphasis" href="#">Science</a>
+      <a classname="nav-item nav-link link-body-emphasis" href="#">Health</a>
+      <a classname="nav-item nav-link link-body-emphasis" href="#">Style</a>
+      <a classname="nav-item nav-link link-body-emphasis" href="#">Travel</a>
+    </nav>
+  </div>
+</div>
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
         <Navbar.Brand as={NavLink} to={"/"}>
@@ -20,7 +56,7 @@ const Navigation = () => {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav classnameName="me-auto">
             {connected && (
               <NavDropdown title="Usuarios" id="basic-nav-dropdown">
                 <NavDropdown.Item href="/registerInternalUser">
@@ -44,7 +80,7 @@ const Navigation = () => {
               </React.Fragment>
             ) : (
               <NavDropdown title={user.sub} id="basic-nav-dropdown">
-                <NavDropdown.Item as={NavLink} to={"#"} className="text-black">
+                <NavDropdown.Item as={NavLink} to={"#"} classnameName="text-black">
                   item
                 </NavDropdown.Item>
                 <NavDropdown.Divider />
@@ -57,6 +93,7 @@ const Navigation = () => {
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    </>
   );
 };
 
