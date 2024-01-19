4559876647a8d46e4ae3d2379227052070a739a8
Jesus Arturo Yañez Patiño <jesusmitesoro2004@gmail.com>
2024-01-19 12:50:53 -0500

    Modificacion de archivos js a jsx

---
+++ src/routes/App.jsx
@@ -0,0 +1,49 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import "moment/locale/es";
+import "react-confirm-alert/src/react-confirm-alert.css";
+import { Navigation } from "../layouts/Navigation";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "../states/store";
+import { getAuthenticationToken } from "../connections/helpers/token";
+import { PrivateRoute } from "./PrivateRoute";
+import { ToastContainer } from "react-toastify";
+
+import { Login } from "../pages/Login";
+import { Register } from "../pages/Register";
+import { Error404 } from "../pages/Error404";
+import { Home } from "../pages/Home";
+import { ObserveUsers } from "../pages/ObserveUsers";
+
+getAuthenticationToken();
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <ToastContainer />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            element={
+              <>
+                <Navigation />
+                <Outlet />
+              </>
+            }
+          />
+          <Route path="/ViewUsers" element={<ObserveUsers />} />
+
+          <Route element={<PrivateRoute />}>
+            <Route path="/Home" element={<Home />} />
+          </Route>
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
+};
+
+export default App;
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,5 +1,8 @@
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
+import { ObserveUsers } from "../pages/ObserveUsers";
+
+
 
 const PrivateRoute = () => {
     const connected = useSelector(state => state.connected);
