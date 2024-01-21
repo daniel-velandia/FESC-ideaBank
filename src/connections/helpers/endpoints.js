bdcc4f37c33e05c52a9739417b2d2d97192c92f7
Jhon-Rodrigurez <116046489+Jhon-Rodrigurez@users.noreply.github.com>
2024-01-20 22:20:53 -0500

    Delete src directory

--- src/connections/helpers/endpoints.js
+++
@@ -1,9 +0,0 @@
-const API_URL = "http://localhost:8080";
-
-export const REGISTER_POST_ENDPOINT = API_URL + "/register";
-export const LOGIN_POST_ENDPOINT = API_URL + "/login";
-
-export const CAREER_ALL_GET_ENDPOINT = API_URL + "/program/get-all";
-export const ROL_ALL_GET_ENDPOINT = API_URL + "/rol/get-all";
-
-export const USER_CREATE_POST_ENDPOINT = API_URL + "/user/create"
