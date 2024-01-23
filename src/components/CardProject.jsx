a8f37e2e1420336e066b35e845488fc3b89c4014
Elkin0Gomez <est_eer_gomez@fesc.edu.co>
2024-01-22 21:44:43 -0500

    Modify card design to observe the projects.

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -1,31 +1,31 @@
 // CardProject.js
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
-import '../css/style.css';
+import "../css/style.css";
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function CardProject({ imageSource, title, text, url }) {
   return (
-    <div className="card text-center bg-dark animate__animated animate__fadeInUp">
-      <div className="overflow">
-        <img src={imageSource} alt="a wallpaper" className="card-img-top" />
-      </div>
-      <div className="card-body text-light">
-        <h4 className="card-title">{title}</h4>
-        <p className="card-text text-secondary">
-          {text
-            ? text
-            : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam deserunt fuga accusantium excepturi quia, voluptates obcaecati nam in voluptas perferendis velit harum dignissimos quasi ex? Tempore repellat quo doloribus magnam."}
-        </p>
-        <a
-          href={url ? url : "#!"}
-          target="_blank"
-          className="btn btn-outline-secondary border-0"
-          rel="noreferrer"
-        >
-          Ver más sobre {title}
-        </a>
-      </div>
-    </div>
+    <Link to={url} className="Card--link">
+      <Card className="Card--featured">
+        <div className="d-flex align-items-center">
+          <Card.Body>
+            <Card.Title>{title}</Card.Title>
+            <Card.Text>
+              {text ? text : "Lorem ipsum dolor sit amet consectetu"}
+            </Card.Text>
+          </Card.Body>
+          <div className="image-container">
+            <Card.Img
+              className="custom-img"
+              variant="bottom"
+              src={imageSource}
+            />
+          </div>
+        </div>
+      </Card>
+    </Link>
   );
 }
 
@@ -33,7 +33,7 @@ CardProject.propTypes = {
   title: PropTypes.string.isRequired,
   text: PropTypes.string,
   url: PropTypes.string,
-  imageSource: PropTypes.string
+  imageSource: PropTypes.string,
 };
 
 export default CardProject;
--- src/pages/project/ProjectList.jsx
+++ src/pages/project/ProjectList.jsx
@@ -2,42 +2,48 @@
 import React from "react";
 import CardProject from "../../components/CardProject";
 import "../../css/style.css";
+import { Container, Row, Col } from "react-bootstrap";
 
-import image3 from "../../img/imagen 3.png";
+import image1 from "../../img/imagen 1.jpg";
+import image2 from "../../img/imagen 2.jpg";
+import image4 from "../../img/imagen 4.jpg";
 
 const cards = [
   {
     id: 1,
-    title: "SIBIES",
-    imageSource: image3,
+    title: "Verde Esperanza",
+    imageSource: image1,
     url: "",
+    text: `Damos la bienvenida a "Verde Esperanza", un proyecto dedicado a la promoción de prácticas sostenibles y al impulso de la conciencia ambiental en nuestra comunidad.`
   },
   {
     id: 2,
-    title: "PROYECTANDO",
-    imageSource: image3,
+    title: "Conectando Mentes, Construyendo Futuros",
+    imageSource: image2,
     url: "",
+    text: `¡Bienvenidos a "Conectando Mentes, Construyendo Futuros"! Este proyecto tiene como objetivo principal derribar barreras y promover la inclusión educativa para todos. `
   },
   {
     id: 3,
-    title: "FESC GO",
-    imageSource: image3,
+    title: "EcoVida",
+    imageSource: image4,
     url: "",
+    text: `Te damos la bienvenida a "EcoVida", un proyecto dedicado a fomentar el desarrollo sostenible en nuestra comunidad.`
   },
 ];
 
 function ProjectList() {
   return (
     <div className="cards-container">
-      <div className="container d-flex justify-content-center align-items-center h-100 mt-5">
-        <div className="row">
-          {cards.map(({ title, imageSource, url, id }) => (
-            <div className="col-md-4" key={id}>
-              <CardProject imageSource={imageSource} title={title} url={url} />
-            </div>
+      <Container className="mt-5">
+        <Row>
+          {cards.map(({ title, imageSource, url, id, text }) => (
+            <Col key={id} xs={12} md={6} lg={6} className="mb-4">
+              <CardProject text={text} imageSource={imageSource} title={title} url={url} />
+            </Col>
           ))}
-        </div>
-      </div>
+        </Row>
+      </Container>
     </div>
   );
 }
