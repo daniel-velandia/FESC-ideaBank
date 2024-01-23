7b120b028072055b8cf057c715abb53a82925de3
Jesus Arturo Yañez Patiño <jesusmitesoro2004@gmail.com>
2024-01-23 12:37:30 -0500

    react toastify implementation to show custom server errors, implemented for login error when entering fake credentials

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,67 +1,86 @@
 import { useEffect, useState } from "react";
 import { Alert, Card, Col, Container, Image, Row } from "react-bootstrap";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { LoginForm } from '../../components/LoginForm';
-import { authentication } from '../../connections/userActions';
+import { LoginForm } from "../../components/LoginForm";
+import { authentication } from "../../connections/userActions";
 import { isEmptyObject } from "../../connections/helpers/isEmptyObject";
 import validator from "validator";
+import ToastError from "../../components/ToastError";
 
 const Login = () => {
+  const [errors, setErrors] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
 
-    const [errors, setErrors] = useState({});
-    const connected = useSelector(state => state.connected);
-    const navigation = useNavigate();
-    const dispatch = useDispatch();
+  const connected = useSelector((state) => state.connected);
+  const navigation = useNavigate();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        if(connected) {
-            navigation('/');
-        }
-    });
+  useEffect(() => {
+    if (connected) {
+      navigation("/");
+    }
+  });
 
-    const login = (user) => {
+  const login = (user) => {
+    setErrorMessage(null);
+    const error = {};
 
-        const error = {};
-
-        if (validator.isEmpty(user.email)) {
-            error.email = 'El correo no puede estar vacio';
-        }
-        
-        if (validator.isEmpty(user.password)) {
-            error.password = 'La contraseña no puede estar vacia';
-        }
-
-        if (!isEmptyObject(error)) {
-            setErrors(error);
-        } else {
-            dispatch(authentication(user))
-            .then(res => navigation('/'))
-            .catch(err => setErrors({ login: 'Credenciales incorrectas' }));
-        }
+    if (validator.isEmpty(user.email)) {
+      error.email = "El correo no puede estar vacio";
     }
 
-    return (
+    if (validator.isEmpty(user.password)) {
+      error.password = "La contraseña no puede estar vacia";
+    }
 
-        <Container>
-            <Row className='flex-container justify-content-md-center'>
-                <Col sm='12' md='4'>
+    if (!isEmptyObject(error)) {
+      setErrors(error);
+    } else {
+      dispatch(authentication(user))
+        .then((res) => navigation("/"))
+        .catch((err) => {
+          setErrorMessage(err.response.data.mensaje);
+        });
+    }
+  };
 
-                    <div className="text-center mt-3 mb-2">
-                        <Image className="mb-4" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" rounded alt="" width="72" height="72" />
-                        <h1 className="h3 mb-3 font-weight-normal">Login</h1>
-                        <p>Ingresa tus credenciales para acceder a la plataforma de manera segura y personalizada.</p>
-                    </div>
+  const renderToastError = () => {
+    if (errorMessage) {
+      return <ToastError message={errorMessage} />;
+    }
+    return null;
+  };
 
-                    <Card.Body>
-                        {errors.login && <Alert variant='danger'>{errors.login}</Alert>}
-                        <LoginForm errors={errors} callback={login} />
-                    </Card.Body>
-                    
-                </Col>
-            </Row>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <Row className="flex-container justify-content-md-center">
+        <Col sm="12" md="4">
+          <div className="text-center mt-3 mb-2">
+            <Image
+              className="mb-4"
+              src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
+              rounded
+              alt=""
+              width="72"
+              height="72"
+            />
+            <h1 className="h3 mb-3 font-weight-normal">Login</h1>
+            <p>
+              Ingresa tus credenciales para acceder a la plataforma de manera
+              segura y personalizada.
+            </p>
+          </div>
+
+          <Card.Body>
+            {errors.login && <Alert variant="danger">{errors.login}</Alert>}
+            <LoginForm errors={errors} callback={login} />
+            {renderToastError()}
+          </Card.Body>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export { Login };
