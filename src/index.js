d8a567558ad34d4f66bb20227003431ace88c2a8
Daniel Velandia <est_da_velandia@fesc.edu.co>
2024-01-27 20:03:23 -0500

    fix navbar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './css/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
+import './css/index.css';
 import App from './routes/App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
--- src/layouts/Navigation.jsx
+++ src/layouts/Navigation.jsx
@@ -3,8 +3,7 @@ import { NavLink } from 'react-router-dom';
 import Navbar from "react-bootstrap/Navbar";
 import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../connections/userActions';
-import { Container, Nav, NavDropdown, SplitButton, Image } from 'react-bootstrap';
-import { FaUser, FaUserPlus } from "react-icons/fa";
+import { Container, Nav, NavDropdown, Image } from 'react-bootstrap';
 import logo from '../img/logo-nav.png';
 import { useEffect } from "react";
 
@@ -12,23 +11,6 @@ function Navigation() {
     const connected = useSelector((state) => state.connected);
     const user = useSelector((state) => state.user);
     const dispatch = useDispatch();
-
-    const circuloEstilo = {
-        width: "30px",
-        height: "30px",
-        borderRadius: "50%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#fff",
-        color: "#e30513",
-        cursor: "pointer",
-        fontWeight: '600',
-    };
-    
-      const dropdownEstilo = {
-        borderRadius: "50%",
-    };
 
     useEffect(() => {
         const links = document.querySelectorAll('.link');
@@ -55,16 +37,14 @@ function Navigation() {
                 <div className="row flex-nowrap justify-content-between align-items-center">
                     <div className="d-flex justify-content-between">
                         <Navbar.Brand as={NavLink} to={"/"} 
-                        className="animate__animated animate__tada text-dark animate__slower 3s animate__delay-1s animate__infinite fs-4 mt-3">
+                        className="text-dark mt-3">
                             <Image src={logo} height={40}/>
                         </Navbar.Brand>
                         {connected ? (
-                            <SplitButton
-                                title={<div style={circuloEstilo}>{user.sub.substring(0, 2).toUpperCase()}</div>}
-                                id="split-button-dropdown"
-                                className='me-0 mt-2 ms-2'
-                                style={dropdownEstilo}
-                                variant="danger"
+                            <NavDropdown
+                                title={<div className='my-avatar' >{user.sub.substring(0, 2).toUpperCase()}</div>}
+                                id="button-dropdown"
+                                className='my-avatar-dropdown'
                                 >
                                 <React.Fragment>
                                     <NavDropdown.Item as={NavLink} to={"/"} className='text-black'>
@@ -75,22 +55,19 @@ function Navigation() {
                                 <NavDropdown.Item onClick={() => dispatch(logout())} className='text-black'>
                                     Cerrar sesión
                                 </NavDropdown.Item>
-                            </SplitButton>
+                            </NavDropdown>
                         ) : ""}
                     </div>
                 </div>
             </header>
             <hr/>
             <div className="nav-scroller py-1 mb-2">
-                <Nav className="nav animate__animated animate__zoomIn animate__slow 1s">
-                    <NavLink to={"/user"} className="link p-2 ms-0 mx-3" id="usuarios-link">
-                        <FaUser className='mx-2' />
-                        <span>Usuarios</span>
+                <Nav className="nav">
+                    <NavLink to={"/"} className="link p-2 ms-0 mx-3" id="crear-usuario-link">
+                        Proyectos
                     </NavLink>
-
-                    <NavLink to={"/user/create"} className="link p-2 ms-0 mx-3" id="crear-usuario-link">
-                        <FaUserPlus className='mx-2' />
-                        <span>Crear usuario</span>
+                    <NavLink to={"/user"} className="link p-2 ms-0 mx-3" id="usuarios-link">
+                        Usuarios
                     </NavLink>
                 </Nav>
             </div>
