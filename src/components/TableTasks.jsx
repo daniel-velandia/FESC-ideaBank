1f29f1b11b0eaaa437dd84cf81a91ae2132d66a8
Jhon-Rodrigurez <est_ja.rodriguez@fesc.edu.co>
2024-02-05 21:40:41 -0500

    Task status displayed

--- src/components/TableTasks.jsx
+++ src/components/TableTasks.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Badge } from 'react-bootstrap';
 
 const TableTasks = ({ task }) => {
   return (
     <tr>
       <td>{task.title}</td>
-      <td>{task.description}</td>
       <td>{task.assignedUser}</td>
-      <td>{task.creationDate}</td>
       <td>{task.finishDate}</td>
+      <td>{task.status === 'EN PROGRESO' ? (
+        <Badge className='my-badge-progress-state'>{task.status}</Badge>
+      ) : task.status === 'PENDIENTE' ? (
+        <Badge className='my-badge-pending-state'>{task.status}</Badge>
+      ) : task.status === 'APROBADO' ? (
+        <Badge className='my-badge-appoved-state'>{task.status}</Badge>
+      ) : task.status === 'LISTO' ? (
+        <Badge className='my-badge-done-state'>{task.status}</Badge>
+      ) : 
+        <Badge className='my-badge-rejected-state'>{task.status}</Badge>
+      }
+      </td>
     </tr>
   );
 };
